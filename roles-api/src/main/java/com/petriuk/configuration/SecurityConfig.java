08b928094b3f8a414e5b8b6cdcf9a4414698f667
petriuk <serhii.petriuk@nixsolutions.com>
2021-02-23 17:02:57 +0200

    [oauth2] set up keycloak at roles-api

---
+++ roles-api/src/main/java/com/petriuk/configuration/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.petriuk.configuration;
+
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@KeycloakConfiguration
+@EnableWebSecurity
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter
+{
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(keycloakAuthenticationProvider());
+    }
+
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new NullAuthenticatedSessionStrategy();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        super.configure(http);
+        http
+            .authorizeRequests()
+            .antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+            .antMatchers("/roles/**").hasAnyRole("ADMIN", "SUPERADMIN")
+        .and().csrf().disable();
+    }
+}
--- roles-api/src/main/java/com/petriuk/controller/RolesRestController.java
+++ roles-api/src/main/java/com/petriuk/controller/RolesRestController.java
@@ -6,6 +6,7 @@ import com.petriuk.service.RolesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,6 +29,7 @@ public class RolesRestController {
         return rolesService.getRoleNameByUserId(userId);
     }
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/roles")
     public List<String> getAllRoles() {
         return rolesService.getAllRoles();
